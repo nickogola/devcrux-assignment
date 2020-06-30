e8cb8862763af50cc358f7f23a025e970a6e0aef
Nick Ogola <ogolanick@gmail.com>
2020-06-30 02:23:04 -0400

    first commit

---
+++ Assignment/Assignment/Controls/ButtonControl.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Assignment.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonControl : ContentView
+    {
+        public ButtonControl()
+        {
+            InitializeComponent();
+            customBtn.Text = "Proceed";
+            customBtn.TextColor = Color.White;
+            customBtn.BackgroundColor = Color.ForestGreen;
+
+            customBtn.Pressed += Button_Pressed;
+        }
+
+        private void Button_Pressed(object sender, EventArgs e)
+        {
+            var customBtn = sender as Button;
+            if (customBtn.Text.Equals("Testing")) {
+                customBtn.Text = "Proceed";
+                customBtn.BackgroundColor = Color.ForestGreen;
+                customBtn.ImageSource = "play.png";
+            }
+            else
+            {
+                customBtn.Text = "Testing";
+                customBtn.BackgroundColor = Color.SteelBlue;
+                customBtn.ImageSource = "next.png";
+            }
+        }
+
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(Title),
+                                                                                        typeof(string),
+                                                                                        typeof(ButtonControl),
+                                                                                        default(string),
+                                                                                        BindingMode.OneWay);
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text),
+                                                                                      typeof(string),
+                                                                                      typeof(ButtonControl),
+                                                                                      default(string),
+                                                                                      BindingMode.TwoWay);
+
+        public static readonly BindableProperty ColorHexProperty = BindableProperty.Create(nameof(ColorHex),
+                                                                                  typeof(string),
+                                                                                  typeof(ButtonControl),
+                                                                                  default(string),
+                                                                                  BindingMode.TwoWay);
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public string ColorHex
+        {
+            get { return (string)GetValue(ColorHexProperty); }
+            set { SetValue(ColorHexProperty, value); }
+        }
+    }
+}
---
+++ Assignment/Assignment/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Assignment
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            userTitle = "Click me";
+            colorHex = "#000333";
+            this.BindingContext = this;
+        }
+
+        private string userTitle;
+
+        public string UserTitle
+        {
+            get { return userTitle; }
+            set { userTitle = value; }
+        }
+
+        private string colorHex;
+
+        public string ColorHex
+        {
+            get { return colorHex; }
+            set { colorHex = value; }
+        }
+
+
+    }
+}
